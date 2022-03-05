a805660f454e2c57dff7060323d3300a6d0c8cc0
eugeniabud <evgenia.zrain@mail.ru>
2022-03-05 22:49:36 +0300

    lr3

--- Program.cs
+++ Program.cs
@@ -1,89 +1,31 @@
-﻿using Google;
-using System;
-using System.Linq;
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+
 
 namespace FirstApp
 {
     class Program
     {
         static void Main(string[] args)
-        {
-            using (helloappdbContext db = new helloappdbContext())
-            {
-                User user1 = new User { Name = "Tom", Age = 33 };
-                User user2 = new User { Name = "Alice", Age = 26 };
-                // Добавление
-                db.Users.Add(user1);
-                db.Users.Add(user2);
-                db.SaveChanges();
+        {           
+                var builder = new ConfigurationBuilder()                
+                .SetBasePath(Directory.GetCurrentDirectory())               
+                .AddJsonFile("appsettings.json");
+                var config = builder.Build();
+
+                string connectionString = config.GetConnectionString("DefaultConnection");
+
+                var optionsBuilder = new DbContextOptionsBuilder<helloappdbContext>();
+                var options = optionsBuilder.UseSqlServer(connectionString).Options;
+                using (helloappdbContext db = new helloappdbContext())
+                {
+                    var users = db.Users.ToList();
+                    foreach (User u in users)
+                    {
+                        Console.WriteLine($"{u.Id}.{u.Name} - {u.Age}");
+                    }
+                }
+                Console.Read();
             }
-            // получение
-            using (helloappdbContext db = new helloappdbContext())
-            {
-                // получаем объекты из БД и выводим на консоль
-                var users = db.Users.ToList();
-                Console.WriteLine("Данные после добавления:");
-                foreach (User u in users)
-                {
-                    Console.WriteLine($"{u.Id}.{u.Name} - {u.Age}");
-                }
-            }
-            // Редактирование
-            using (helloappdbContext db = new helloappdbContext())
-            {
-                // получаем первый объект
-                User user = db.Users.FirstOrDefault();
-                if (user != null)
-                {
-                    user.Name = "Bob";
-                    user.Age = 44;
-                    //обновляем объект
-                    db.Users.Update(user);
-                    db.SaveChanges();
-                }
-                // выводим данные после обновления
-                Console.WriteLine("\nДанные после редактирования:");
-                var users = db.Users.ToList();
-                foreach (User u in users)
-                {
-                    Console.WriteLine($"{u.Id}.{u.Name} - {u.Age}");
-                }
-            }
-            // Удаление
-            using (helloappdbContext db = new helloappdbContext())
-            {
-                // получаем первый объект
-                User user = db.Users.FirstOrDefault();
-                if (user != null)
-                {
-                    //удаляем объект
-                    db.Users.Remove(user);
-                    db.SaveChanges();
-                }
-                // выводим данные после обновления
-                Console.WriteLine("\nДанные после удаления:");
-                var users = db.Users.ToList();
-                foreach (User u in users)
-                {
-                    Console.WriteLine($"{u.Id}.{u.Name} - {u.Age}");
-                }
-            }
-            Console.Read();
         }
     }
-}
-
-//            using (helloappdbContext db = new helloappdbContext())
-//            {
-//                // получаем объекты из БД и выводим на консоль
-//                var users = db.Users.ToList();
-//                Console.WriteLine("Список объектов:");
-//                foreach (User u in users)
-//                {
-//                    Console.WriteLine($"{u.Id}.{u.Name} - {u.Age}");
-//                }
-//            }
-//            Console.ReadKey();
-//        }
-//    }
-//}
